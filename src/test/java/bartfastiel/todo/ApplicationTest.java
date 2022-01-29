af26eb8dc60a5ba5840f276f0978cbad440112a5
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2022-01-29 17:40:57 +0100

    Enable CI build (GitHub Actions)
    Flapdoodle download a mongodb installation file depending on the os (win/linux/mac), the bit-level (32bit/64bit) and the mongo version (i.e. 5.0.5). GitHub's os distributions seem not to work with mongo version `latest`. Local test runs in windows 64bit fail, if explicitly set to `5.0.5`.
    
    Implemented Workaround: using a spring profile to toggle between GitHub test runs and local test runs.

--- src/test/java/bartfastiel/todo/ApplicationTest.java
+++ src/test/java/bartfastiel/todo/ApplicationTest.java
@@ -13,11 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest(
-        properties = {
-                "spring.mongodb.embedded.version=latest",
-        }
-)
+@SpringBootTest
 @AutoConfigureMockMvc
 public class ApplicationTest {
 
